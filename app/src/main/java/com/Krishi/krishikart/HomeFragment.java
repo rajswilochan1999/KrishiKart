7f25d497f8d9fa3b06cbbba4f6d0f0bcd81a69f2
kalpe <nitp0000@gmail.com>
2020-01-16 22:38:42 +0530

    sixth

--- app/src/main/java/com/Krishi/krishikart/HomeFragment.java
+++ app/src/main/java/com/Krishi/krishikart/HomeFragment.java
@@ -1,16 +1,40 @@
 package com.Krishi.krishikart;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    HomeAdapter homeAdapter;
+    ArrayList<PojoLinear> list=new ArrayList<>();
+
+    ProgressDialog progressDialog;
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReference().child("images");
+    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference().child("Post");
 
     public HomeFragment() {
         // Required empty public constructor
@@ -18,10 +42,40 @@ public class HomeFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View root=inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        final View root=inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView=root.findViewById(R.id.homerecyclerview);
+        recyclerView.hasFixedSize();
+        layoutManager=new LinearLayoutManager(this.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        progressDialog = new ProgressDialog(this.getContext());
+        progressDialog.setMessage("Please wait...");
+        progressDialog.show();
+        try {
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+
+                    for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+
+                        PojoLinear pojoLinear = postSnapshot.getValue(PojoLinear.class);
+                        list.add(pojoLinear);
+                    }
+                    homeAdapter=new HomeAdapter(getContext(),list);
+                    recyclerView.setAdapter(homeAdapter);
+                    progressDialog.dismiss();
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    progressDialog.dismiss();
+                    Toast.makeText(root.getContext(), "Failed "+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (Exception e ) {
+            progressDialog.dismiss();
+            Toast.makeText(root.getContext(), "Failed!! "+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
         return root;
     }
 
---
+++ app/src/main/java/com/Krishi/krishikart/PojoLinear.java
@@ -0,0 +1,70 @@
+package com.Krishi.krishikart;
+
+public class PojoLinear {
+    private String productimage;
+    private String productname;
+    private String username;
+    private String phonenumber;
+    private String productquantity;
+    private String pricerange;
+
+    public  PojoLinear() {
+    }
+
+    public PojoLinear(String productimage, String productname, String username, String phonenumber, String productquantity, String pricerange) {
+        this.productimage = productimage;
+        this.productname = productname;
+        this.username = username;
+        this.phonenumber = phonenumber;
+        this.productquantity = productquantity;
+        this.pricerange = pricerange;
+    }
+
+    public String getProductimage() {
+        return productimage;
+    }
+
+    public void setProductimage(String productimage) {
+        this.productimage = productimage;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getProductquantity() {
+        return productquantity;
+    }
+
+    public void setQuantity(String productquantity) {
+        this.productquantity = productquantity;
+    }
+
+    public String getPricerange() {
+        return pricerange;
+    }
+
+    public void setPricerange(String pricerange) {
+        this.pricerange = pricerange;
+    }
+}
