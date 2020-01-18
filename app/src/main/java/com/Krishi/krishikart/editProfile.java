6019faa5979bc792029f3d3f95a1a01ec7e19d77
anukul <iamanukulkumar262001@gmail.com>
2020-01-18 13:43:42 +0530

    anukul2

--- app/src/main/java/com/Krishi/krishikart/editProfile.java
+++ app/src/main/java/com/Krishi/krishikart/editProfile.java
@@ -1,5 +1,7 @@
 package com.Krishi.krishikart;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,13 +14,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class editProfile extends AppCompatActivity {
 
+    private CircleImageView ProfileImage;
     EditText editName,editPhone,editAddress;
     Button saveChange;
     DatabaseReference reff;
     User user=new User();
+    private static final int REQUEST_CODE=23;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==REQUEST_CODE && resultCode ==RESULT_OK){
+
+            if(data!=null){
+                Uri uri=data.getData();
+
+                Toast.makeText(this,"Uri"+uri,Toast.LENGTH_LONG).show();
+                Toast.makeText(this,"Path"+uri.getPath(),Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,10 +52,19 @@ public class editProfile extends AppCompatActivity {
         editAddress=findViewById(R.id.editAddress);
         editPhone=findViewById(R.id.editPhone);
         saveChange=findViewById(R.id.saveChanges);
-//
-//        String Name=editName.getText().toString();
-//        String Address=editAddress.getText().toString();
-//        String Phone=editPhone.getText().toString();
+        ProfileImage=(CircleImageView)findViewById(R.id.profile);
+
+        ProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.setType("images/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent,REQUEST_CODE);
+
+            }
+        });
+
 
 
 
@@ -46,6 +78,7 @@ public class editProfile extends AppCompatActivity {
                user.setPhone(editPhone.getText().toString().trim());
                reff.push().setValue(user);
                Toast.makeText(editProfile.this,"data inserted Successfully",Toast.LENGTH_SHORT).show();
+
 
            }
        });
