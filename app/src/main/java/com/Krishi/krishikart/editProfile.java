6277a95f63e99d414fb6daca8f40e894802af3fe
anukul <iamanukulkumar262001@gmail.com>
2020-01-18 17:39:42 +0530

    anukul2

--- app/src/main/java/com/Krishi/krishikart/editProfile.java
+++ app/src/main/java/com/Krishi/krishikart/editProfile.java
@@ -1,44 +1,63 @@
 package com.Krishi.krishikart;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.FileUtils;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class editProfile extends AppCompatActivity {
 
+    public static final int RESULT_LOAD_IMAGE=1;
     private CircleImageView ProfileImage;
     EditText editName,editPhone,editAddress;
     Button saveChange;
     DatabaseReference reff;
     User user=new User();
-    private static final int REQUEST_CODE=23;
+    private static final int PICK_IMAGE=100;
+    Uri imageUri;
+    FirebaseAuth mauth;
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK&&requestCode==PICK_IMAGE){
+            imageUri=data.getData();
+            ProfileImage.setImageURI(imageUri);
+            StorageReference ref= FirebaseStorage.getInstance().getReference().child("profilePictures/").child(mauth.getUid());
+            ref.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
 
-        if(requestCode==REQUEST_CODE && resultCode ==RESULT_OK){
 
-            if(data!=null){
-                Uri uri=data.getData();
 
-                Toast.makeText(this,"Uri"+uri,Toast.LENGTH_LONG).show();
-                Toast.makeText(this,"Path"+uri.getPath(),Toast.LENGTH_SHORT).show();
 
-            }
+                }
+            });
 
         }
     }
@@ -48,40 +67,35 @@ public class editProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_profile_edit);
-        editName=findViewById(R.id.editName);
-        editAddress=findViewById(R.id.editAddress);
-        editPhone=findViewById(R.id.editPhone);
-        saveChange=findViewById(R.id.saveChanges);
-        ProfileImage=(CircleImageView)findViewById(R.id.profile);
+        editName = findViewById(R.id.editName);
+        editAddress = findViewById(R.id.editAddress);
+        editPhone = findViewById(R.id.editPhone);
+        saveChange = findViewById(R.id.saveChanges);
+        ProfileImage = (CircleImageView) findViewById(R.id.profile);
 
         ProfileImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Intent.ACTION_OPEN_DOCUMENT);
-                intent.setType("images/*");
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                startActivityForResult(intent,REQUEST_CODE);
-
-            }
+                Intent gallery=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(gallery,PICK_IMAGE);
+              }
         });
 
 
+        reff = FirebaseDatabase.getInstance().getReference().child("User");
+        saveChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                user.setName(editName.getText().toString());
+                user.setAddress(editAddress.getText().toString().trim());
+                user.setPhone(editPhone.getText().toString().trim());
+                reff.push().setValue(user);
+                Toast.makeText(editProfile.this, "data inserted Successfully", Toast.LENGTH_SHORT).show();
 
 
-       reff= FirebaseDatabase.getInstance().getReference().child("User");
-       saveChange.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-
-               user.setName(editName.getText().toString());
-               user.setAddress(editAddress.getText().toString().trim());
-               user.setPhone(editPhone.getText().toString().trim());
-               reff.push().setValue(user);
-               Toast.makeText(editProfile.this,"data inserted Successfully",Toast.LENGTH_SHORT).show();
-
-
-           }
-       });
+            }
+        });
     }
 }
 
